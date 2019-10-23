db911f0561d59868d46f7c3a732302984d7b740c
JoelTraverso <54408217+JoelTraverso@users.noreply.github.com>
2019-10-23 00:13:49 -0300

    Add files via upload

--- menuHabitaciones.h
+++ menuHabitaciones.h
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<stdio.h>
 #include<time.h>
+#include"string.h"
 #define MENUHABITACIONES_H_INCLUDED
 
 
@@ -30,6 +31,7 @@ public:
         }
      }
 };*/
+
 class FechaSistema
 {
 private:
@@ -80,7 +82,33 @@ public:
     {
         return minuto;
     }
+
 };
+bool comparar_fechas(char ini[11],char fin[11],int a,int m,int d)
+{
+    //if((a-1900>=strcat(ini[8],ini[9])-'0')&&(m>=strcat(ini[3],ini[4])-'0')&&(d>=strcat(ini[0],ini[1])-'0')&&(a-1900<=strcat(fin[8],fin[9])-'0')&&(m<=strcat(fin[3],fin[4])-'0')&&(d<=strcat(fin[0],fin[1])-'0'))
+    int di1,di2,mi1,mi2,ai1,ai2,df1,df2,mf1,mf2,af1,af2;
+    di1=ini[0]-'0';
+    di2=ini[1]-'0';
+    mi1=ini[3]-'0';
+    mi2=ini[4]-'0';
+    ai1=ini[8]-'0';
+    ai2=ini[9]-'0';
+    df1=fin[0]-'0';
+    df2=fin[1]-'0';
+    mf1=fin[3]-'0';
+    mf2=fin[4]-'0';
+    af1=fin[8]-'0';
+    af2=fin[9]-'0';
+    if((a-2000)>=((ai1*10)+ai2)&&(m>=((mi1*10)+mi2))&&(d>=(di1*10)+di2))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 class Gasto
 {
 private:
@@ -156,7 +184,6 @@ void Gasto::cargar()
     }
     fseek(g,0,2);
     id=((ftell(g)/sizeof(*this))+1);
-    cout<<id;
     cout<<"Ingrese la habitacion que realizo el gasto: ";
     cin>>habitacion;
     cout<<"Ingrese el tipo de gasto: ";
@@ -175,23 +202,61 @@ void Gasto::cargar()
 void Gasto::consulta_habitacion()
 {
     FILE *g;
+    char cons[4];
     g=fopen("gastos.dat","rb");
     if(g==NULL)
     {
         cout<<"Fallo al abrir el archivo";
         return;
     }
+    cout<<"Ingrese la habitacion por la que quiere consultar: ";
+    cin>>cons;
     while(fread(this,sizeof(*this),1,g)==1)
     {
-        cout<<id<<endl;
-        cout<<habitacion<<endl;
-        cout<<tipo<<endl;
-        cout<<importe<<endl;
-        cout<<descripcion<<endl<<endl;
-        cout<<"-------------------------------"<<endl;
+        if(strcmp(habitacion,cons)==0)
+        {
+            cout<<id<<endl;
+            cout<<habitacion<<endl;
+            cout<<tipo<<endl;
+            cout<<importe<<endl;
+            cout<<descripcion<<endl<<endl;
+            cout<<fec.mostar_dia()<<"/"<<fec.mostar_mes()<<"/"<<fec.mostar_anio()<<"  "<<fec.mostar_hora()<<":"<<fec.mostar_minuto()<<endl;
+            cout<<"-------------------------------"<<endl;
+        }
+    }
+    fclose(g);
+}
+
+void Gasto::consulta_habitacion_fecha()
+{
+    FILE *g;
+    char cons[4],fecini[11],fecfin[11];
+    g=fopen("gastos.dat","rb");
+    if(g==NULL)
+    {
+        cout<<"Fallo al abrir el archivo";
+        return;
+    }
+    cout<<"Ingrese la habitacion por la que quiere consultar: ";
+    cin>>cons;
+    cout<<"Ingrese fecha de inicio de la busqueda";
+    cin>>fecini;
+    cout<<"Ingrese fecha de fin de la busqueda";
+    cin>>fecfin;
+    while(fread(this,sizeof(*this),1,g)==1)
+    {
+        if(strcmp(habitacion,cons)==0&&comparar_fechas(fecini,fecfin,fec.mostar_anio(),fec.mostar_mes(),fec.mostar_dia())==true)
+        {
+            cout<<id<<endl;
+            cout<<habitacion<<endl;
+            cout<<tipo<<endl;
+            cout<<importe<<endl;
+            cout<<descripcion<<endl<<endl;
+            cout<<fec.mostar_dia()<<"/"<<fec.mostar_mes()<<"/"<<fec.mostar_anio()<<"  "<<fec.mostar_hora()<<":"<<fec.mostar_minuto()<<endl;
+            cout<<"-------------------------------"<<endl;
+        }
     }
     fclose(g);
 }
 
 #endif // MENUHABITACIONES_H_INCLUDED
-
